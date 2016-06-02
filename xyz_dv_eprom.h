7a236ad754716bd4eb026ac917d41422f90194c0
Mutant22 <kozovsky.radoslav@gmail.com>
2016-06-02 15:08:57 +0200

    Update xyz_dv_eprom.h

--- xyz_dv_eprom.h
+++ xyz_dv_eprom.h
@@ -98,9 +98,9 @@ void setup_io()
 #define Serial_println(x) printf("%s\n", x)
 #define digitalWrite(x, y)
 
-typedef boolean bool
-typedef byte (unsigned char)
-typedef word (unsigned short)
+typedef boolean bool;
+typedef byte (unsigned char);
+typedef word (unsigned short);
 
 //RPI macros and configuration - end
 
